6324c474ded3ad78067975e5cc69a3ed8daae62c
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-02-13 11:56:41 -0300

    #fix: fazendo o site sempre navegar para a página principal

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,11 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./home";
+
+function RedirectToHome() {
+  return <Navigate to="/" replace />;
+}
+
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/">
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<RedirectToHome />} />
       </Route>
     </Routes>
   )
